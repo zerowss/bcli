fdd47e5be4f047ffbea4ea78613eff8b6fcc2f5a
wangsongsong <wangsongsong@meiliwu.com>
2018-11-09 17:55:48 +0800

    change bcli

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@
  * @Author: wangss 
  * @Date: 2018-11-08 15:25:14 
  * @Last Modified by: wangss
- * @Last Modified time: 2018-11-09 16:24:48
+ * @Last Modified time: 2018-11-09 17:30:25
  */
 module.exports = {
   lintOnSave: false,
+  assetsDir: "./",
   devServer: {
     port: 8088, // 端口号
     host: "localhost",
