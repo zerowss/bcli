da69549019ca0b6f53d54b5bb6404091ac530798
wangsongsong <wangsongsong@meiliwu.com>
2019-01-10 10:43:31 +0800

    a

--- src/framework/login/script.js
+++ src/framework/login/script.js
@@ -16,10 +16,17 @@ export default {
     resetClick(num) {
       this.activity = num;
     },
-    async weChatLogin() {
+    weChatLogin() {
+      const a =
+        "https://open.weixin.qq.com/connect/qrconnect?appid=wx39c379788eb1286a&scope=snsapi_login&redirect_uri=http%3A%2F%2Fmp.weixin.qq.com%2Fdebug%2Fcgi-bin%2Fsandbox%3Ft%3Dsandbox%2Flogin";
+      this.getUrl().then(url => {
+        window.location.href = url ? url : a;
+      });
+    },
+    async getUrl() {
       const res = await this.$http.get("/api/wechat/redirect", {});
       if (res.data && res.data.code == 0) {
-        window.location.href = res.data.url;
+        return res.data.url;
       }
     }
   }
